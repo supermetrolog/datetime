f316e8eb1ee5794d35345ff8d1ee5e1286f538bc
Timur <billypro6@gmail.com>
2022-11-01 12:14:45 +0300

    test 2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ namespace app;
 
 use app\controllers\DatetimeController;
 use app\lib\response\ErrorResponse;
-//testdsda
+//testdsdasdawdddddddddddddd
 require __DIR__ . '/vendor/autoload.php';
 header('Content-Type: application/json; charset=utf-8');
 try {
