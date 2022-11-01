e322827048accd212b404dacc9d2c624b21a24ef
Timur <billypro6@gmail.com>
2022-11-01 12:11:25 +0300

    test usernmae

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ namespace app;
 
 use app\controllers\DatetimeController;
 use app\lib\response\ErrorResponse;
-
+//test
 require __DIR__ . '/vendor/autoload.php';
 header('Content-Type: application/json; charset=utf-8');
 try {
